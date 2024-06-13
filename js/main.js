b420f1b0d74474ed8ebcac7405366dde61ee0b82
zvMateo <zavalamateo14@gmail.com>
2024-06-13 13:23:58 -0300

    POSTS cargados desde JS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,55 @@
 $(document).ready(function(){
+    
+    //Slider
+    
     $('.galeria').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200,
         responsive: true,
+    });
+
+    //Posts
+    var posts = [
+        {
+            title: 'Prueba de titulo 1',
+            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illum qui delectus eius pariatur, optio quas. Officia iure fugit odit laboriosam id praesentium atque maxime quas. Earum sapiente deserunt, quia labore architecto, perspiciatis ex cupiditate possimus id saepe inventore nam nisi! Optio a, aliquam at accusantium consequuntur doloremque ducimus fuga!' 
+        },
+        {
+            title: 'Prueba de titulo 2',
+            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illum qui delectus eius pariatur, optio quas. Officia iure fugit odit laboriosam id praesentium atque maxime quas. Earum sapiente deserunt, quia labore architecto, perspiciatis ex cupiditate possimus id saepe inventore nam nisi! Optio a, aliquam at accusantium consequuntur doloremque ducimus fuga!' 
+        },
+        {
+            title: 'Prueba de titulo 3',
+            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illum qui delectus eius pariatur, optio quas. Officia iure fugit odit laboriosam id praesentium atque maxime quas. Earum sapiente deserunt, quia labore architecto, perspiciatis ex cupiditate possimus id saepe inventore nam nisi! Optio a, aliquam at accusantium consequuntur doloremque ducimus fuga!' 
+        },
+        {
+            title: 'Prueba de titulo 4',
+            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illum qui delectus eius pariatur, optio quas. Officia iure fugit odit laboriosam id praesentium atque maxime quas. Earum sapiente deserunt, quia labore architecto, perspiciatis ex cupiditate possimus id saepe inventore nam nisi! Optio a, aliquam at accusantium consequuntur doloremque ducimus fuga!' 
+        },
+        {
+            title: 'Prueba de titulo 5',
+            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus illum qui delectus eius pariatur, optio quas. Officia iure fugit odit laboriosam id praesentium atque maxime quas. Earum sapiente deserunt, quia labore architecto, perspiciatis ex cupiditate possimus id saepe inventore nam nisi! Optio a, aliquam at accusantium consequuntur doloremque ducimus fuga!' 
+        },
+    ];
+
+    posts.forEach((item, index) => {
+        var post = `
+            <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>
+                    ${item.content}
+                </p>
+                <a href="#" class="button-more">Leer más</a>
+            </article>
+        `;
+
+        $("#posts").append(post);
     });
 });
